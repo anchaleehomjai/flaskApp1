95883b5259ef4391ccaedf3fd66af7be9536ca09
anchaleehomjai <anchalee_homjai@cmu.ac.th>
2023-02-26 13:47:20 +0700

    Lab12

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -2,16 +2,14 @@ import os
 from flask import Flask
 from werkzeug.debug import DebuggedApplication
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_login import LoginManager
 
 app = Flask(__name__, static_folder='static')
 
-
 # this DEBUG config here will be overridden by FLASK_DEBUG shell environment
 app.config['DEBUG'] = True
-app.config['SECRET_KEY'] = 'a8112ea716969327fc2a49fc8dd0e2ca9fa484033e771552'
+app.config['SECRET_KEY'] = 'ae7b9205c7a88aafaeb8f4a66493c3531e63df8ef9a9a770'
 app.config['JSON_AS_ASCII'] = False
-
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", "sqlite://")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -22,5 +20,8 @@ if app.debug:
 # Creating an SQLAlchemy instance
 db = SQLAlchemy(app)
 
+login_manager = LoginManager()
+login_manager.login_view = 'lab12_login'
+login_manager.init_app(app)
 
 from app import views  # noqa
--- web/manage.py
+++ web/manage.py
@@ -1,15 +1,12 @@
 from flask.cli import FlaskGroup
-
-
+from werkzeug.security import generate_password_hash
 from app import app, db
 from app.models.contact import Contact
 from app.models.blogentry import BlogEntry
-
+from app.models.authuser import AuthUser
 
 
 cli = FlaskGroup(app)
-
-
 
 
 @cli.command("create_db")
@@ -19,10 +16,10 @@ def create_db():
     db.session.commit()
 
 
-
-
 @cli.command("seed_db")
 def seed_db():
+    db.session.add(AuthUser(email="flask@204212", name='สมชาย ทรงแบด',password=generate_password_hash('1234',method='sha256'),avatar_url='https://ui-avatars.com/api/?name=\
+สมชาย+ทรงแบด&background=83ee03&color=fff'))
     db.session.add(
         Contact(firstname='สมชาย', lastname='ทรงแบด', phone='081-111-1111'))
     db.session.commit()
